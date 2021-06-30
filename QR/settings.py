7c70f89e25f159bdc9f57e27258ce4787d94b55f
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-07-01 00:57:12 +0100

    commit

--- QR/settings.py
+++ QR/settings.py
@@ -141,18 +141,19 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = AWS_URL + '/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_URL = AWS_URL + '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIAFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
