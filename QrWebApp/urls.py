f714cf817ff748ff96710679a099e9932dd7472c
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-06-30 16:02:07 +0100

    commit

--- QrWebApp/urls.py
+++ QrWebApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from QrWebApp.views import home, post_url
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('home/post/', post_url, name='post'),
+    path('', home, name='home'),
+    path('post/', post_url, name='post'),
 ]
